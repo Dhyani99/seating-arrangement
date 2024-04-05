fac33c240569f6e513c7046af877c34a98b98b17
Dhyani99 <gandhidhyani@gmail.com>
2024-04-04 21:25:00 -0500

    testing front end

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     fetch("https://seating-arrangement-api-production.up.railway.app/message")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
+
+    console.log(message)
   }, []);
 
   return (
